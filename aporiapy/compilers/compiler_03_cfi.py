38a76f0b52efedcce3faf22fabde6237295e2786
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-02-22 09:21:57 +0100

    implement support for floats

--- aporiapy/compilers/compiler_03_cfi.py
+++ aporiapy/compilers/compiler_03_cfi.py
@@ -9,29 +9,33 @@ class CompilerCfi:
     def compile(self, program_ast: Module) -> L_cfi:
         int_vars = set()
         bool_vars = set()
+        float_vars = set()
         ap_stmts = []
         for stmt in program_ast.body:
             if not isinstance(stmt, ast.If):
                 raise Exception("Only if statements are allowed in the program")
-            ap_stmts.append(self.translate_stmt(stmt, int_vars, bool_vars))
+            ap_stmts.append(self.translate_stmt(stmt, int_vars, bool_vars, float_vars))
 
         int_vars = {Var(var) for var in int_vars}
         bool_vars = {Var(var) for var in bool_vars}
+        float_vars = {Var(var) for var in float_vars}
         ap_declar = []
         if not len(int_vars) == 0:
             ap_declar.append(Declar(Int(), int_vars))
         if not len(bool_vars) == 0:
             ap_declar.append(Declar(Bool(), bool_vars))
+        if not len(float_vars) == 0:
+            ap_declar.append(Declar(Float(), float_vars))
         cfi = L_cfi(ap_declar, ap_stmts)
         return cfi
 
-    def translate_stmt(self, if_stmt, int_vars, bool_vars) -> Stmt:
+    def translate_stmt(self, if_stmt, int_vars, bool_vars, float_vars) -> Stmt:
         test = if_stmt.test
         ap_test = self.select_pred(test)
         body = if_stmt.body
         if not body or len(body) != 1:
             raise Exception("Body of if statement must have exactly one statement")
-        ap_body = self.select_instruction(body[0], int_vars, bool_vars)
+        ap_body = self.select_instruction(body[0], int_vars, bool_vars, float_vars)
         return Stmt(None, ap_test, ap_body)
 
     def select_pred(self, test) -> Pred:
@@ -43,34 +47,39 @@ class CompilerCfi:
             case _:
                 raise Exception(f"Unexpected test in select_pred: {test}")
 
-    def select_instruction(self, stmt, int_vars, bool_vars) -> Inst:
+    def select_instruction(self, stmt, int_vars, bool_vars, float_vars) -> Inst:
         match stmt:
             case ast.Expr(Call(Name('print'), [exp])):
                 return PrintInst("", self.select_exp(exp))
             case ast.Assign([Name(var)], exp):
-                self.check_type(var, exp, int_vars, bool_vars)
+                self.check_type(var, exp, int_vars, bool_vars, float_vars)
                 return AssignInst(lcfi_ast.Assign(Var(var), self.select_exp(exp)))
             case ast.Expr(value):
                 return ExpInst(self.select_exp(value))
             case _:
                 raise Exception(f"Unexpected statement in select_instruction: {stmt}")
 
-
-    def check_type(self, new_var, exp, int_vars, bool_vars):
+    def check_type(self, new_var, exp, int_vars, bool_vars, float_vars):
         match exp:
             case ast.Name(var):
-                if var in int_vars:
-                    int_vars.add(new_var)
-                elif var in bool_vars:
+                if var in bool_vars:
                     bool_vars.add(new_var)
+                elif var in int_vars:
+                    if new_var not in float_vars:
+                        int_vars.add(new_var)
+                elif var in float_vars:
+                    if new_var in int_vars:
+                        int_vars.remove(new_var)
+                    float_vars.add(new_var)
                 else:
                     raise Exception(f"Variable {var} not declared")
-            case ast.BinOp():
-                int_vars.add(new_var)
+            case ast.BinOp(left, op, right):
+                self.check_type(new_var, left, int_vars, bool_vars, float_vars)
+                self.check_type(new_var, right, int_vars, bool_vars, float_vars)
             case ast.BoolOp():
                 bool_vars.add(new_var)
-            case ast.UnaryOp(ast.USub(), _):
-                int_vars.add(new_var)
+            case ast.UnaryOp(ast.USub(), exp):
+                self.check_type(new_var, exp, int_vars, bool_vars, float_vars)
             case ast.UnaryOp(ast.Not(), _):
                 bool_vars.add(new_var)
             case ast.Compare():
@@ -79,7 +88,14 @@ class CompilerCfi:
                 if isinstance(value, bool):
                     bool_vars.add(new_var)
                 elif isinstance(value, int):
-                    int_vars.add(new_var)
+                    if new_var not in float_vars:
+                        int_vars.add(new_var)
+                elif isinstance(value, float):
+                    if new_var in int_vars:
+                        int_vars.remove(new_var)
+                    float_vars.add(new_var)
+                else:
+                    raise Exception(f"Unexpected constant: {value}")
             case _:
                 raise Exception(f"Unexpected expression in check_type: {exp}")
 
