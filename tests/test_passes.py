7736ffc73cf47612c9e2e42013d8db6f1cb78f5a
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-03-11 08:11:58 +0100

    update tests

--- tests/test_passes.py
+++ tests/test_passes.py
@@ -8,12 +8,11 @@ from aporiapy.compilers.compiler_01_sc import CompilerSc
 from aporiapy.compilers.compiler_02_flat import CompilerFlat
 from aporiapy.compilers.compiler_03_cfi import CompilerCfi
 
-compilers = [CompilerSc(), CompilerFlat(), CompilerCfi()]
 
 TEST_DIR = Path(__file__).parent / "test_cases"
 
-def execute_and_capture(code):
-    """Executes a Python code string and captures stdout."""
+def execute_and_capture(program_ast):
+    code = ast.unparse(program_ast)
     output = io.StringIO()
     try:
         with contextlib.redirect_stdout(output):
@@ -22,6 +21,13 @@ def execute_and_capture(code):
         return output.getvalue().rstrip("\n")
     except Exception as e:
         return f"Error: {e}"
+
+
+compilers_interpreter_pairs = [
+    (CompilerSc, execute_and_capture),
+    (CompilerFlat, execute_and_capture),
+    (CompilerCfi, interpreter.InterpLcfi().interp)
+]
 
 # Load all test cases
 def load_test_cases():
@@ -32,16 +38,14 @@ def load_test_cases():
 def test_compiler(test_file):
     """Tests the compiler on multiple programs."""
     original_source = test_file.read_text().strip()
-    output_original = execute_and_capture(original_source)
     program_ast = ast.parse(original_source)
+    output_original = execute_and_capture(program_ast)
 
-    for compiler in compilers:
-        program_ast = compiler.compile(program_ast)
+    for (compiler, interpret) in compilers_interpreter_pairs:
+        program_ast = compiler().compile(program_ast)
         if isinstance(program_ast, ast.Module):
             ast.fix_missing_locations(program_ast)
-            compiled_source = ast.unparse(program_ast)
-            output_compiled = execute_and_capture(compiled_source)
-        else:
-            output_compiled = interpreter.InterpLcfi().interp(program_ast)
-        assert output_compiled == output_original, f"FAIL ({test_file.name}, {compiler.__class__.__name__}): expected {output_original} but got {output_compiled}"
+        output_compiled = interpret(program_ast)
+        assert output_compiled == output_original,\
+            f"FAIL ({test_file.name}, {compiler.__class__.__name__}): expected {output_original} but got {output_compiled}"
 
