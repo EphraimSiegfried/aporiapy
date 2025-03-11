f4b96823f67c6f3fc6eb755398f61e5e1847e8ae
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-03-11 08:11:13 +0100

    improve/correct compilation of floats

--- aporiapy/compilers/compiler_03_cfi.py
+++ aporiapy/compilers/compiler_03_cfi.py
@@ -1,43 +1,39 @@
 import ast
 from ast import *
+from collections import defaultdict
 
 import aporia.aporia_ast as lcfi_ast
 from aporia.aporia_ast import *
+
+from aporiapy.compilers.utils import generate_name
 
 
 class CompilerCfi:
 
     def compile(self, program_ast: Module) -> L_cfi:
-        int_vars = set()
-        bool_vars = set()
-        float_vars = set()
+        var_to_type = dict()
         ap_stmts = []
         for stmt in program_ast.body:
             if not isinstance(stmt, ast.If):
                 raise Exception("Only if statements are allowed in the program")
-            ap_stmts.append(self.translate_stmt(stmt, int_vars, bool_vars, float_vars))
+            ap_stmts.extend(self.translate_stmt(stmt, var_to_type))
 
-        int_vars = {Var(var) for var in int_vars}
-        bool_vars = {Var(var) for var in bool_vars}
-        float_vars = {Var(var) for var in float_vars}
-        ap_declar = []
-        if not len(int_vars) == 0:
-            ap_declar.append(Declar(Int(), int_vars))
-        if not len(bool_vars) == 0:
-            ap_declar.append(Declar(Bool(), bool_vars))
-        if not len(float_vars) == 0:
-            ap_declar.append(Declar(Float(), float_vars))
+        type_to_var = defaultdict(set)
+        for var, var_type in var_to_type.items():
+            type_to_var[var_type].add(Var(var))
+
+        ap_declar = [Declar(var_type(), var_set) for var_type, var_set in type_to_var.items()]
+
         cfi = L_cfi(ap_declar, ap_stmts)
         return cfi
 
-    def translate_stmt(self, if_stmt, int_vars, bool_vars, float_vars) -> Stmt:
-        test = if_stmt.test
-        ap_test = self.select_pred(test)
+    def translate_stmt(self, if_stmt, var_to_type) -> list[Stmt]:
+        pred = self.select_pred(if_stmt.test)
         body = if_stmt.body
         if not body or len(body) != 1:
             raise Exception("Body of if statement must have exactly one statement")
-        ap_body = self.select_instruction(body[0], int_vars, bool_vars, float_vars)
-        return Stmt(None, ap_test, ap_body)
+        aux_stmts, ap_body = self.select_instruction(body[0], var_to_type)
+        return aux_stmts + [Stmt(None, pred, ap_body)]
 
     def select_pred(self, test) -> Pred:
         match test:
@@ -48,94 +44,129 @@ class CompilerCfi:
             case _:
                 raise Exception(f"Unexpected test in select_pred: {test}")
 
-    def select_instruction(self, stmt, int_vars, bool_vars, float_vars) -> Inst:
+    def select_instruction(self, stmt, var_to_type) -> tuple[list[Stmt], Inst]:
         match stmt:
             case ast.Expr(ast.Call(ast.Name("print"), args)):
                 if isinstance(string := getattr(args[0], "value", None), str):
-                    expression = None
+                    exp = None
+                    aux_stmts = []
                     if len(args) > 1:
-                        expression = self.select_exp((args[1]))
+                        aux_stmts, exp = self.select_exp(args[1], var_to_type)
                         string += " "
-                    return PrintInst(string, expression)
+                    return aux_stmts, PrintInst(string, exp)
                 else:
-                    return PrintInst("", self.select_exp(args[0]))
+                    aux_stmts, exp = self.select_exp(args[0], var_to_type)
+                    return aux_stmts, PrintInst("", exp)
             case ast.Assign([Name(var)], exp):
-                self.check_type(var, exp, int_vars, bool_vars, float_vars)
-                return AssignInst(lcfi_ast.Assign(Var(var), self.select_exp(exp)))
+                self.check_type(var, exp, var_to_type)
+                aux_stmts, exp = self.select_exp(exp, var_to_type)
+                return aux_stmts, AssignInst(lcfi_ast.Assign(Var(var), exp))
             case ast.Expr(value):
-                return ExpInst(self.select_exp(value))
+                aux_stmts, exp = self.select_exp(value, var_to_type)
+                return aux_stmts, ExpInst(exp)
             case _:
                 raise Exception(f"Unexpected statement in select_instruction: {stmt}")
 
-    def check_type(self, new_var, exp, int_vars, bool_vars, float_vars):
+    def check_expression_type(self, exp, var_to_type):
         match exp:
+            case ast.Constant(bool()):
+                return lcfi_ast.Bool
+            case ast.Constant(int()):
+                return lcfi_ast.Int
+            case ast.Constant(float()):
+                return lcfi_ast.Float
             case ast.Name(var):
-                if var in bool_vars:
-                    bool_vars.add(new_var)
-                elif var in int_vars:
-                    if new_var not in float_vars:
-                        int_vars.add(new_var)
-                elif var in float_vars:
-                    if new_var in int_vars:
-                        int_vars.remove(new_var)
-                    float_vars.add(new_var)
-                else:
-                    raise Exception(f"Variable {var} not declared")
-            case ast.BinOp(left, op, right):
-                if isinstance(op, ast.Div):
-                    float_vars.add(new_var)
-                    if new_var in int_vars:
-                        int_vars.remove(new_var)
-                else:
-                    self.check_type(new_var, left, int_vars, bool_vars, float_vars)
-                    self.check_type(new_var, right, int_vars, bool_vars, float_vars)
+                if not var in var_to_type:
+                     raise Exception(f"Undefined variable detected in check_type: {var}")
+                return var_to_type[var]
             case ast.BoolOp():
-                bool_vars.add(new_var)
-            case ast.UnaryOp(ast.USub(), exp):
-                self.check_type(new_var, exp, int_vars, bool_vars, float_vars)
+                return lcfi_ast.Bool
             case ast.UnaryOp(ast.Not(), _):
-                bool_vars.add(new_var)
+                return lcfi_ast.Bool
             case ast.Compare():
-                bool_vars.add(new_var)
-            case ast.Constant(value):
-                if isinstance(value, bool):
-                    bool_vars.add(new_var)
-                elif isinstance(value, int):
-                    if new_var not in float_vars:
-                        int_vars.add(new_var)
-                elif isinstance(value, float):
-                    if new_var in int_vars:
-                        int_vars.remove(new_var)
-                    float_vars.add(new_var)
+                return lcfi_ast.Bool
+            case ast.BinOp(_, ast.Div(), _):
+                return lcfi_ast.Float
+            case ast.BinOp(_, ast.FloorDiv(), _):
+                return lcfi_ast.Int
+            case ast.BinOp(left, op, right):
+                left_type = self.check_expression_type(left, var_to_type)
+                right_type = self.check_expression_type(right, var_to_type)
+                operand_types = [left_type, right_type]
+                if any(operand_type == lcfi_ast.Float for operand_type in operand_types):
+                    return lcfi_ast.Float
+                elif all(operand_type == lcfi_ast.Int for operand_type in operand_types):
+                    return lcfi_ast.Int
+                elif all(operand_type == lcfi_ast.Bool for operand_type in operand_types):
+                    return lcfi_ast.Bool
                 else:
-                    raise Exception(f"Unexpected constant: {value}")
+                    raise Exception(f"Unexpected operand type in check_type: {left} {op} {right}")
+            case ast.UnaryOp(ast.USub(), exp):
+                return self.check_expression_type(exp, var_to_type)
             case _:
                 raise Exception(f"Unexpected expression in check_type: {exp}")
 
-    def select_exp(self, exp) -> Exp:
+    def check_type(self, new_var, exp, var_to_type):
+        exp_type = self.check_expression_type(exp, var_to_type)
+        if not new_var in var_to_type:
+            var_to_type[new_var] = exp_type
+        elif var_to_type[new_var] == lcfi_ast.Int and exp_type == lcfi_ast.Float:
+            var_to_type[new_var] = lcfi_ast.Float
+        elif exp_type == var_to_type[new_var] or (exp_type == lcfi_ast.Int and var_to_type[new_var] == lcfi_ast.Float):
+            return
+        else:
+            raise Exception(f"Cannot cast {var_to_type[new_var]} to {exp_type} in statement: {new_var} = {exp}")
+
+    def to_float(self, exp, var_to_type):
+        new_stmt = None
+        if isinstance(exp, lcfi_ast.Constant):
+            new_exp = Constant(float(exp.value))
+        else:
+            name = generate_name("temp")
+            new_exp = lcfi_ast.Var(name)
+            var_to_type[name] = lcfi_ast.Float
+            to_float_cast_instr = lcfi_ast.AssignInst(lcfi_ast.Assign(new_exp, exp))
+            new_stmt = lcfi_ast.Stmt(None, lcfi_ast.Pred(Bools(True)), to_float_cast_instr)
+        return new_stmt, new_exp
+
+    def select_exp(self, exp, var_to_type) -> tuple[list[Stmt], Exp]:
         match exp:
             case ast.Name(var):
-                return Var(var)
+                return [], Var(var)
             case ast.Constant(value):
-                return (
+                return [], (
                     lcfi_ast.Bools(value)
                     if isinstance(value, bool)
                     else lcfi_ast.Constant(value)
                 )
             case ast.BinOp(left, op, right):
-                op = self.select_op(op)
-                return lcfi_ast.BinOp(self.select_exp(left), op, self.select_exp(right))
+                new_op = self.select_op(op)
+                operand_types = [self.check_expression_type(o, var_to_type) for o in [left, right]]
+                aux_left, left = self.select_exp(left, var_to_type)
+                aux_right, right = self.select_exp(right, var_to_type)
+                new_operands = [left, right]
+                aux_stmts = aux_left + aux_right
+                if (self.check_expression_type(exp, var_to_type) == lcfi_ast.Float
+                        and all(type == lcfi_ast.Int for type in operand_types)):
+                    index = next((i for i, o in enumerate(new_operands) if isinstance(o,lcfi_ast.Constant)), 0)
+                    aux_stmt, new_operands[index] = self.to_float(new_operands[index], var_to_type)
+                    if aux_stmt:
+                        aux_stmts.append(aux_stmt)
+                return aux_stmts, lcfi_ast.BinOp(new_operands[0], new_op, new_operands[1])
             case ast.UnaryOp(op, operand):
                 op = self.select_op(op)
-                return lcfi_ast.UnaryOp(op, self.select_exp(operand))
+                aux_stmts, operand = self.select_exp(operand, var_to_type)
+                return aux_stmts, lcfi_ast.UnaryOp(op, operand)
             case ast.BoolOp(op, [left, right]):
                 op = self.select_op(op)
-                return lcfi_ast.BinOp(self.select_exp(left), op, self.select_exp(right))
+                aux_left, left = self.select_exp(left, var_to_type)
+                aux_right, right = self.select_exp(right, var_to_type)
+                return aux_left + aux_right, lcfi_ast.BinOp(left, op, right)
             case ast.Compare(left, [cmp], [right]):
                 cmp = self.select_cmp(cmp)
-                return lcfi_ast.BinOp(
-                    self.select_exp(left), cmp, self.select_exp(right)
-                )
+                aux_left, left = self.select_exp(left, var_to_type)
+                aux_right, right = self.select_exp(right, var_to_type)
+                return aux_left + aux_right, lcfi_ast.BinOp(left, cmp, right)
             case _:
                 raise Exception(f"Unexpected expression in select_exp: {exp}")
 
